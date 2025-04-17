909e54d650ef18203a138308168dd15089937e4c
dynalz <tonyramos237@hotmail.com>
2025-04-17 13:52:25 +0100

    :hammer:

--- scripts/shell.py
+++ scripts/shell.py
@@ -1,10 +1,15 @@
 import IPython
 from traitlets.config import Config
 
+"""
+No use other than opening shell with ipython and test stuff
+"""
+
 
 def shell():
     CONFIG = Config()
     CONFIG.TerminalInteractiveShell.autoindent = False
+    CONFIG.InteractiveShellApp.exec_lines = ["from caching import cache"]
 
     IPython.start_ipython(config=CONFIG)
 
