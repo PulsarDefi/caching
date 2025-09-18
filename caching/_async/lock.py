68e459dbd5ce2c46682a7e109cc499966b5326dd
PedroMMRAF <pedro.mmraf@gmail.com>
2025-09-18 16:53:37 +0100

    sort imports

--- caching/_async/lock.py
+++ caching/_async/lock.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import DefaultDict
 from collections import defaultdict
+from typing import DefaultDict
 
 _ASYNC_LOCKS: DefaultDict[str, DefaultDict[str, asyncio.Lock]] = defaultdict(lambda: defaultdict(asyncio.Lock))
